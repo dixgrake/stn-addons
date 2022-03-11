d179da1564a88b1145ec07f75ebe990b72695c7e
Dix Grake <dixgrake@gmail.com>
2022-03-11 23:50:51 +0000

    [14.0][FIX] academy-invoice: fix duplicate content

--- academy-invoice/__manifest__.py
+++ academy-invoice/__manifest__.py
@@ -20,8 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
-    'depends': ['academy'],
+    'depends': ['base', 'academy'],
 
     # always loaded
     'data': [
