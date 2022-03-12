d58119b3c11d70e05b13aa84f48d7f84393f1212
Dix Grake <dixgrake@gmail.com>
2022-03-12 00:19:58 +0000

    [14.0][REF] academy:  add summary manifest

--- academy/__manifest__.py
+++ academy/__manifest__.py
@@ -3,8 +3,9 @@
     'name': "Academy",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+    Este aplicacion le permitara gestionar los procesos de un centros de educacion, Universidad
+    Escuelas de nivel primaria y secundaria.
+        """,
 
     'description': """
         Long description of module's purpose
